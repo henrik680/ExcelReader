eff248adab40ec98c327b6fe9e940b5362dbe7b5
henrik <henrik.martensson@gmail.com>
2021-02-01 20:22:39 +0100

    FIrst version

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import openpyxl as excel
+import requests
+
+
+def openExelUrl(url):
+    print(url)
+    s = requests.get(url).content
+    xl = pd.read_excel(s, engine='openpyxl')
+    print("done")
+    return xl
+
+
+fileUrl = "http://epi6.energimyndigheten.se/SharePoint/Eugen/Godkända anläggningar.xlsx"
+df = openExelUrl(fileUrl)
+print(df.head(n=10))
